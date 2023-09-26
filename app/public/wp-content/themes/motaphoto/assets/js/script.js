3c037305b38368ea674d8a702eff2b48d04d92c2
Elisa Gruau <lisale.kurosaki@gmail.com>
2023-09-26 12:29:51 +0200

    Animation menu burger

--- app/public/wp-content/themes/motaphoto/assets/js/script.js
+++ app/public/wp-content/themes/motaphoto/assets/js/script.js
@@ -1,36 +1,40 @@
 // MENU BURGER //
+
+const headerNav = document.querySelector('.header__nav');
+const bodyContent = document.querySelector('body');
+const logo = document.querySelector('.header__logo__desktop');
+
+
 function openMenu() {
-    document.querySelector('.header__nav').classList.add('header__nav__open');
-    document.querySelector('.header__burger').classList.add('header__burger__icone');
+    headerNav.classList.add('header__nav__open');
+    headerNav.classList.remove('header__nav__closed');
+    bodyContent.classList.add('overlay-animation');
+    bodyContent.classList.remove('overlay-animation__closed');
+    logo.classList.add('opacity-animation');
+    logo.classList.remove('opacity-animation__closed');
 }
 
 function closeMenu() {
-    document.querySelector('.header__nav').classList.remove('header__nav__open');
-    document.querySelector('.header__burger').classList.remove('header__burger__icone');
+    headerNav.classList.remove('header__nav__open');
+    headerNav.classList.add('header__nav__closed');
+    bodyContent.classList.remove('overlay-animation');
+    bodyContent.classList.add('overlay-animation__closed');
+    logo.classList.remove('opacity-animation');
+    logo.classList.add('opacity-animation__closed');
 }
 
-// let links = document.querySelectorAll('.header__nav__menu > li');
-// console.log(links);
-
-// links.forEach(link => {
-//   link.addEventListener("click", closeMenu);
-// });
 
 // GESTION DE LA MODALE //
 document.addEventListener('wpcf7submit', function(event) {
-  // Code pour agrandir la modale en cas d'erreur
+  // Code pour agrandir la modale à l'envoie du formulaire
   var modal = document.getElementById('modal-content');
   modal.style.height = '900px';
-  console.log("La modale est plus grande")
 }, false);
 
 // Get the modal
-// Votre code ici
   let modal = document.querySelector('.modal');
-console.log(modal);
 
 let btnContact = document.querySelector('.contactLink');
-console.log(btnContact);
 btnContact.onclick = function() {
   modal.style.display = "block";
 }
@@ -40,15 +44,3 @@ window.onclick = function(event) {
       modal.style.display = "none";
   }
 }
-
-
-
-// Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-
-// When the user clicks on the button, open the modal
-// btn.onclick = function() {
-//     modal.style.display = "block";
-// }
-
-// When the user clicks anywhere outside of the modal, close it
--- app/public/wp-content/themes/motaphoto/header.php
+++ app/public/wp-content/themes/motaphoto/header.php
@@ -12,7 +12,7 @@
     
     <?php wp_body_open(); ?>
        <header class="header">
-              <div class="header__logo" </div> <?php if ( function_exists( 'the_custom_logo' ) ) {
+              <div class="header__logo header__logo__desktop" <?php if ( function_exists( 'the_custom_logo' ) ) {
               the_custom_logo(); } ?> </div> 
               <div class="header__burger" onclick="openMenu()">
                                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="19" viewBox="0 0 28 19"
@@ -24,23 +24,27 @@
                                           <path d="M26.5586 17.1345H0.856708C0.383774 17.1345 0 17.5184 0 17.9912C0 18.4642 0.383845 18.8479 0.856708 18.8479H26.5586C27.0315 18.8479 27.4153 18.4641 27.4153 17.9912C27.4154 17.5183 27.0315 17.1345 26.5586 17.1345Z"
                                                  fill="black" />
                                    </svg>
+              </div>
+              <nav class="header__nav">
+                     <div class="header__nav__mobile">
+                            <div class="header__logo header__nav__logo" <?php if ( function_exists( 'the_custom_logo' ) ) {
+                            the_custom_logo(); } ?> </div> 
+                            <div class="header__nav__icone__closed" onclick="closeMenu()">
+                                   <svg xmlns="http://www.w3.org/2000/svg" width="22" height="19" viewBox="0 0 22 19"
+                                          fill="none">
+                                          <path d="M20.1905 0.734096L0.860267 16.9341C0.504576 17.2322 0.45818 17.7625 0.756595 18.1177C1.05505 18.4729 1.58593 18.5192 1.94157 18.2212L21.2718 2.02116C21.6275 1.72307 21.6739 1.19284 21.3755 0.83764C21.0771 0.482341 20.5462 0.436002 20.1905 0.734096Z"
+                                          fill="black" />
+                                          <path d="M0.644737 1.94409L19.9778 18.1465C20.3335 18.4447 20.8641 18.3986 21.1622 18.0439C21.4602 17.6891 21.4134 17.1592 21.0577 16.8611L1.72462 0.65872C1.36893 0.360628 0.838326 0.406552 0.540259 0.761338C0.242191 1.11612 0.289047 1.646 0.644737 1.94409Z"
+                                          fill="black" />
+                                   </svg>
                             </div>
-              <nav class=header__nav>
-                     <div class="header__nav__icone__closed" onclick="closeMenu()">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="19" viewBox="0 0 22 19"
-                                   fill="none">
-                                   <path d="M20.1905 0.734096L0.860267 16.9341C0.504576 17.2322 0.45818 17.7625 0.756595 18.1177C1.05505 18.4729 1.58593 18.5192 1.94157 18.2212L21.2718 2.02116C21.6275 1.72307 21.6739 1.19284 21.3755 0.83764C21.0771 0.482341 20.5462 0.436002 20.1905 0.734096Z"
-                                   fill="black" />
-                                   <path d="M0.644737 1.94409L19.9778 18.1465C20.3335 18.4447 20.8641 18.3986 21.1622 18.0439C21.4602 17.6891 21.4134 17.1592 21.0577 16.8611L1.72462 0.65872C1.36893 0.360628 0.838326 0.406552 0.540259 0.761338C0.242191 1.11612 0.289047 1.646 0.644737 1.94409Z"
-                                   fill="black" />
-                            </svg>
                      </div>
                      <?php
                      if ( has_nav_menu( 'header-menu' ) ) : ?>
                                           <?php 
                      wp_nav_menu ( array (
                      'theme_location' => 'header-menu' ,
-                     'menu_class' => 'header__nav__menu', // classe CSS pour customiser mon menu
+                     'menu_class' => 'header__nav__menu', 
                      'container' => false,
                      ) ); ?>
                                           <?php endif;
